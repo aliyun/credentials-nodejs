8c14239b15f6ea4c7dee2fdf257bebfe4df8d74e
Jackson Tian <shyvo1987@gmail.com>
2020-02-04 17:40:25 +0800

    avoid import json

--- src/util/helper.ts
+++ src/util/helper.ts
@@ -1,5 +1,8 @@
 import * as os from 'os';
-import * as pkg from '../../package.json';
+import kitx from 'kitx';
+import path from 'path';
+
+const pkg = kitx.loadJSONSync(path.join(__dirname, '../../package.json'));
 
 export const DEFAULT_UA = `AlibabaCloud (${os.platform()}; ${os.arch()}) ` +
     `Node.js/${process.version} Core/${pkg.version}`;
